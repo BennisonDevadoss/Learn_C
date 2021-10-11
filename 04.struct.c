03de1a8c76aa780894943774e14c44847eee6082
Bennison Devadoss <bennisondevadoss@gmail.com>
2021-10-11 21:54:26 +0530

    error fixing

---
+++ 04.struct.c
@@ -0,0 +1,16 @@
+//this program we will allocate memory dinamicaly
+
+#include <stdio.h>
+
+//struct data allocationi
+struct course
+{
+    int mark;
+    char subject[30];
+};
+
+int main()
+{
+    //variable declaration
+    struct coures *ptr;
+}
