3422d680001bb73313d7a93a75f108b1918ad008
Bennison Devadoss <bennisondevadoss@gmail.com>
2021-10-11 12:47:59 +0530

    first release

---
+++ 03.writefile.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    //variable declaration
+    char ch[500];
+    FILE *fptr; //file pointer declaration
+
+    //file open
+    fptr = fopen("prog.txt", "w");
+
+    //if file dont open program send this message
+    if (fptr == NULL)
+    {
+        printf("ERROR!\n");
+        printf("FILE did not created");
+    }
+
+    printf("enter the notes");
+    fgets(ch, sizeof(ch), stdin);
+
+    //print input to file
+    fprintf(fptr, "%s", ch);
+    fclose(fptr); //file close
+}
